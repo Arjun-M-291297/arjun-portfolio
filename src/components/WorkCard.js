937358dab7a8ce76c1cc15ef2cee5b6d8bf606b6
Arjun Mepparambath <Arjun.Mepparambath@tanla.com>
2023-07-26 01:41:59 +0530

    chat-app added in workcard

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./WorkCardStyles.css"
 import pro1 from "../assets/pro1.jpg"
+import pro2 from "../assets/pro2.jpg"
 import { NavLink } from 'react-router-dom'
 const WorkCard = () => {
   return (
@@ -13,6 +14,16 @@ const WorkCard = () => {
                 {/* <p>This is text</p> */}
                 <div className='pro-btns'>
                     <NavLink to="https://sweet-boba-e71c72.netlify.app/" target="_blank" rel="noopener noreferrer" className="btn">VIEW</NavLink>
+                </div>
+            </div>
+        </div>
+        <div className='project-card'>
+            <img src={pro2} alt=""></img>
+            <h2 className='project-title'>Chat App</h2>
+            <div className='pro-details'>
+                {/* <p>This is text</p> */}
+                <div className='pro-btns'>
+                    <NavLink to="https://fastidious-pithivier-9b412a.netlify.app/" target="_blank" rel="noopener noreferrer" className="btn">VIEW</NavLink>
                 </div>
             </div>
         </div>
